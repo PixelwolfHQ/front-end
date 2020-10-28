e1fb8bd6fac9a141cd5f455e947860408c6f4e57
João Alberto <joao.oliveira@pixelwolf.com.br>
2020-10-28 13:20:08 -0300

    Updates
    - Adding prettier configs

---
+++ packages/prettier-config/README.md
@@ -0,0 +1,21 @@
+# @pixelwolf/prettier-config
+
+## Installation
+
+`npm i @pixelwolf/prettier-config --save-dev`
+
+## Usage
+
+On `.prettierrc.js` file add:
+
+```json
+module.exports = require("@pixelwolf/prettier-config");
+```
+
+#### Script in package.json
+
+```json
+"scripts": {
+  "prettier": "prettier \"src/**/*.+(ts|tsx|js|jsx|json|yml|yaml|md|mdx)\" --write"
+},
+```
---
+++ packages/prettier-config/index.js
@@ -0,0 +1,18 @@
+module.exports = {
+  arrowParens: "always",
+  bracketSpacing: true,
+  embeddedLanguageFormatting: "auto",
+  htmlWhitespaceSensitivity: "css",
+  insertPragma: false,
+  jsxBracketSameLine: false,
+  jsxSingleQuote: false,
+  printWidth: 120,
+  proseWrap: "preserve",
+  quoteProps: "consistent",
+  requirePragma: false,
+  semi: false,
+  singleQuote: false,
+  tabWidth: 2,
+  trailingComma: "es5",
+  useTabs: false,
+};
