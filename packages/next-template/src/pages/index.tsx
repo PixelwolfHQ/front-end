231a007662a4c97815f03df8e9a6f170d6e406cb
João Alberto <joao.oliveira@pixelwolf.com.br>
2021-01-13 18:11:16 -0300

    Export Theme provider from ui

--- packages/next-template/src/pages/index.tsx
+++ packages/next-template/src/pages/index.tsx
@@ -1,9 +1,11 @@
 import Image from "next/image"
+import { Heading } from '@pixelwolfhq/ui'
 
 export default function Index(): JSX.Element {
   return (
     <div className="pixelwolf">
       <Image src="/images/logo.svg" alt="Pixelwolf" width={241} height={42} />
+      <Heading as="h1">Pixelwolf</Heading>
     </div>
   )
 }
